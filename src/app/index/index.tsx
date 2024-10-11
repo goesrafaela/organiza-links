8772f92cc82f32d771e4cd826066b7243bf94e95
goesrafaela <goes.rafaelasilva@gmail.com>
2024-10-11 19:45:56 -0300

    implementação do logo e do botão de adicionar

---
+++ src/app/index/index.tsx
@@ -0,0 +1,20 @@
+import {Image, View, TouchableOpacity} from 'react-native';
+import {styles} from './styles'
+import { MaterialIcons } from '@expo/vector-icons';
+import { colors } from '@/styles/colors';
+
+export default function Index(){
+    
+    return(
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={require("@/assets/logo.png")} style={styles.logo}/>
+                <TouchableOpacity activeOpacity={0.2}>
+                <MaterialIcons name='add' size={32} color={colors.green[300]}/>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+
