8ca37a2029d1364b232a95615989adfdeaebea88
goesrafaela <goes.rafaelasilva@gmail.com>
2024-10-15 13:51:06 -0300

    inserção da modal de descrição

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,4 +1,4 @@
-import {Image, View, TouchableOpacity, FlatList} from 'react-native';
+import {Image, View, TouchableOpacity, FlatList, Modal, Text} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 //Componentes e estilos criados
@@ -30,7 +30,27 @@ export default function Index(){
                     url='https://rocketseat.com.br/' 
                     onDetails={()=> console.log('clicou')}
                     />)}
+                style={styles.links}
+                contentContainerStyle={styles.linksContent}
+                showsVerticalScrollIndicator={false}
             />
+
+            <Modal transparent visible={true}>
+                <View style={styles.modal}>
+                    <View style={styles.modalContent}>
+                        <View style={styles.modalHeader}>
+                            <Text style={styles.modalCategory}>Curso</Text>
+                            <TouchableOpacity>
+                            <MaterialIcons name="close" size={20} color={colors.gray[400]}/>
+                            </TouchableOpacity>
+                        </View>
+                        <Text style={styles.modalLinkName}>RocketSeat</Text>
+
+                        <Text style={styles.modalUrl}>https://rocketseat.com.br/</Text>
+                    </View>
+                </View>
+            </Modal>
+            
         </View>
     );
 }
