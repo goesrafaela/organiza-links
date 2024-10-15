4677d1df79a7fe6f3c5b85ac756027fa5e339d1c
goesrafaela <goes.rafaelasilva@gmail.com>
2024-10-15 14:10:13 -0300

    criação do componete Options

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -6,6 +6,7 @@ import { colors } from '@/styles/colors';
 import {styles} from './styles'
 import { Categories } from '@/components/categories';
 import { Link } from '@/components/link';
+import { Option } from '@/components/option';
 
 
 export default function Index(){
@@ -45,8 +46,12 @@ export default function Index(){
                             </TouchableOpacity>
                         </View>
                         <Text style={styles.modalLinkName}>RocketSeat</Text>
-
                         <Text style={styles.modalUrl}>https://rocketseat.com.br/</Text>
+
+                        <View style={styles.modalFooter}>
+                            <Option name='Excluir' icon="delete" variant='secondary'/>
+                            <Option name='Abrir' icon="language" />
+                        </View>
                     </View>
                 </View>
             </Modal>
