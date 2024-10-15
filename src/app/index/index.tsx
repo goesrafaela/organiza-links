806118eb097c3e84b1875e29bc4adf12418b60e7
goesrafaela <goes.rafaelasilva@gmail.com>
2024-10-14 21:15:24 -0300

    criando os components(link)

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,7 +1,11 @@
 import {Image, View, TouchableOpacity} from 'react-native';
-import {styles} from './styles'
 import { MaterialIcons } from '@expo/vector-icons';
+
+//Componentes e estilos criados
 import { colors } from '@/styles/colors';
+import {styles} from './styles'
+import { Categories } from '@/components/categories';
+import { Link } from '@/components/link';
 
 export default function Index(){
     
@@ -9,10 +13,13 @@ export default function Index(){
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={require("@/assets/logo.png")} style={styles.logo}/>
-                <TouchableOpacity activeOpacity={0.2}>
+                
+                <TouchableOpacity activeOpacity={0.3}>
                 <MaterialIcons name='add' size={32} color={colors.green[300]}/>
                 </TouchableOpacity>
             </View>
+            <Categories/>
+            <Link name='Rocketseat' url='https://rocketseat.com.br/' onDetails={()=> console.log('clicou')}/>
         </View>
     );
 }
